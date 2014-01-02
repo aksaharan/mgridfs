45c6bd4272bff4b9819a48c6ccbc787a07121c92
Anil Kumar <aksaharan@yahoo.com>
2014-01-02 14:01:02 +0000

    Added test_main for building test-run application

---
+++ test_main.cpp
@@ -0,0 +1,6 @@
+
+int main(int argc, char* argv[], char* arge[]) {
+	// place holder for running cppunit tests for some of the classes that are independent of the 
+	// installed functions etc
+	return 0;
+}
